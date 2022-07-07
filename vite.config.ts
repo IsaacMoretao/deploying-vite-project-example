d0e43fae5de264ac07f49cd48e73274d66d3c608
Isaac Moretão <91755254+IsaacMoretao@users.noreply.github.com>
2022-07-07 11:32:03 -0300

    Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/deploying-vite-project-example/',
+  base: '/portfolio/',
   plugins: [react()]
 })
